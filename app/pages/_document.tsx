942bdc3b55f91f3f918974a301b26e2155d67b85
Musab <musabkhadim363@gmail.com>
2025-05-21 15:04:20 +0500

    update

---
+++ app/pages/_document.tsx
@@ -0,0 +1,29 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Search Console Verification */}
+          <meta
+            name="google-site-verification"
+            content="google1578af168b9d05d6.html"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
